65d446cbb44b134159121d353f8108abe10a18a9
sriram-gandi <90152606+sriram-gandi@users.noreply.github.com>
2021-10-01 01:58:19 +0530

    Update sudoku_solver.py

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -1,11 +1,9 @@
-from pprint import pprint
-
 def isvalid(puzzle,guess,row,col):
     for i in range(0,9):
         if guess == puzzle[row][i]:
             return False
     for i in range(0,9):
-        if guess == puzzel[i][col]:
+        if guess == puzzle[i][col]:
             return False
     begin_row = (row//3)*3
     begin_col = (col//3)*3
@@ -23,19 +21,18 @@ def next_empty(puzzle):
     return -1,-1
 
 def sudoku_solver(puzzle):
-    row,col = next_empty(puzzel)
+    row,col = next_empty(puzzle)
     if row == -1:
         return true
     for guess in range(1,10):
         if isvalid(puzzle,guess,row,col):
             puzzle[row][col] == guess
             if sudoku_solver(puzzle):
-                return True
+                return True,puzzle
     puzzle[row][col] == 0
     return False
     
-puzzle = [[4,0,0,0,0,8,0,0,0][0,0,0,0,9,1,0,8,0][0,8,6,5,0,2,0,3,0][0,2,0,4,0,0,9,0,0][0,1,0,2,0,0,0,0,6][3,6,7,0,5,9,0,0,0][0,0,0,0,0,5,0,0,0][7,0,0,8,0,0,0,2,4][2,0,0,9,3,0,0,7,0]]
+puzzle = [[4,0,0,0,0,8,0,0,0],[0,0,0,0,9,1,0,8,0],[0,8,6,5,0,2,0,3,0],[0,2,0,4,0,0,9,0,0],[0,1,0,2,0,0,0,0,6],[3,6,7,0,5,9,0,0,0],[0,0,0,0,0,5,0,0,0],[7,0,0,8,0,0,0,2,4],[2,0,0,9,3,0,0,7,0]]
             
     
 print(sudoku_solver(puzzle))
-pprint(puzzle)
