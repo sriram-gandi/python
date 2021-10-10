a76c1e53259e3326e38e363b5c79f178d6b7b08c
sriram-gandi <90152606+sriram-gandi@users.noreply.github.com>
2021-10-10 22:57:08 +0530

    Update sudoku_solver.py

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -1,18 +1,29 @@
+#this function checks the guess number valid or not 
 def isvalid(puzzle,guess,row,col):
+    #checking the column for guess number
+    #if the number is found returns false
     for i in range(0,9):
         if guess == puzzle[row][i]:
             return False
+    #checking the row for guess number
+    #if the number is found returns false
     for i in range(0,9):
         if guess == puzzle[i][col]:
             return False
     begin_row = (row//3)*3
     begin_col = (col//3)*3
+    #checking the block for guess number
+    #if the number is found returns false
     for i in range(begin_row, begin_row+3):
         for j in range(begin_col, begin_col+3):
             if guess == puzzle[i][j]:
                 return False
     return True
-    
+#next_empty returns the coordinates of the next empty block
+
+#the sudoku_solver solves sudoku from starting element
+
+#this function returns the next element to be solved 
 def next_empty(puzzle):
     for i in range(0,9):
         for j in range(0,9):
@@ -20,6 +31,8 @@ def next_empty(puzzle):
                 return i,j
     return -1,-1
 
+#sudoku_solver function calls recursively itself
+#it solves the sudoku by using back_tracking
 def sudoku_solver(puzzle):
     row,col = next_empty(puzzle)
     if row == -1:
@@ -32,7 +45,7 @@ def sudoku_solver(puzzle):
     puzzle[row][col] = 0
     return False
     
-puzzle = [[4,0,0,0,0,8,0,0,0],[0,0,0,0,9,1,0,8,0],[0,8,6,5,0,2,0,3,0],[0,2,0,4,0,0,9,0,0],[0,1,0,2,0,0,0,0,6],[3,6,7,0,5,9,0,0,0],[0,0,0,0,0,5,0,0,0],[7,0,0,8,0,0,0,2,4],[2,0,0,9,3,0,0,7,0]]
+puzzle = [[5,3,0,0,7,0,0,0,0],[6,0,0,1,9,5,0,0,0],[0,9,8,0,0,0,0,6,0],[8,0,0,0,6,0,0,0,3],[4,0,0,8,0,3,0,0,1],[7,0,0,0,2,0,0,0,6],[0,6,0,0,0,0,2,8,0],[0,0,0,4,1,9,0,0,5],[0,0,0,0,8,0,0,7,9]]
             
     
 print(sudoku_solver(puzzle))
