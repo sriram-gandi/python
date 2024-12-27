39d8b201bbe35ecf24046dcd75aa940975742894
sriram-gandi <90152606+sriram-gandi@users.noreply.github.com>
2024-12-27 11:30:32 +0530

    Update knighttour.py

--- knighttour.py
+++ knighttour.py
@@ -1,8 +1,1 @@
-knight_help(n,board,x,y,count):
-    if count == n*n:
-        return True
-    if x<0 or y<0 or x>=n or y>=n or board[x][y] !=-1 :
-        return False
-    board[x][y]=count
-    for x_move,y_move in zip[][]
-    if knight_help(n,board,)  
+
